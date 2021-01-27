18ac5302540129ae23bab2420fcf244cad5e0825
patschuh <46817726+patschuh@users.noreply.github.com>
2021-01-27 11:40:06 +0100

    Add more MessageTypes

--- src/main/java/at/esque/kafka/serialization/KafkaEsqueSerializer.java
+++ src/main/java/at/esque/kafka/serialization/KafkaEsqueSerializer.java
@@ -4,15 +4,24 @@ import at.esque.kafka.MessageType;
 import at.esque.kafka.cluster.TopicMessageTypeConfig;
 import at.esque.kafka.handlers.ConfigHandler;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.Serializer;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.utils.Bytes;
 
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.Map;
+import java.util.UUID;
+import java.util.function.Function;
 
 public class KafkaEsqueSerializer implements Serializer<Object> {
 
     private KafkaAvroSerializer avroSerialier = new KafkaAvroSerializer();
-    private StringSerializer stringSerializer = new StringSerializer();
+
+    private Map<MessageType, SerializerWrapper> serializerMap = new EnumMap<MessageType, SerializerWrapper>(MessageType.class) {{
+        Arrays.stream(MessageType.values()).filter(type -> !type.equals(MessageType.AVRO)).forEach(type -> put(type, serializerByType(type)));
+    }};
 
     private String clusterId;
     private boolean isKey;
@@ -27,14 +36,15 @@ public class KafkaEsqueSerializer implements Serializer<Object> {
         if (isKey ? topicConfig.getKeyType().equals(MessageType.AVRO) : topicConfig.getValueType().equals(MessageType.AVRO)) {
             return avroSerialier.serialize(s, object);
         } else {
-            return stringSerializer.serialize(s, (String) object);
+            SerializerWrapper serializerWrapper = serializerMap.get(isKey ? topicConfig.getKeyType() : topicConfig.getValueType());
+            return serializerWrapper.serializer.serialize(s, serializerWrapper.function.apply(object));
         }
     }
 
 
     public void configure(Map<String, ?> configs, boolean isKey) {
         this.isKey = isKey;
-        stringSerializer.configure(configs, isKey);
+        serializerMap.values().forEach(serializer -> serializer.serializer.configure(configs, isKey));
         if (configs.get("schema.registry.url") != null) {
             avroSerialier.configure(configs, isKey);
         }
@@ -43,6 +53,42 @@ public class KafkaEsqueSerializer implements Serializer<Object> {
     }
 
     public void close() {
+    }
+
+    private SerializerWrapper serializerByType(MessageType type) {
+        switch (type) {
+            case STRING:
+                return new SerializerWrapper<String>(s->s, Serdes.String().serializer());
+            case SHORT:
+                return new SerializerWrapper<Short>(Short::parseShort, Serdes.Short().serializer());
+            case INTEGER:
+                return new SerializerWrapper<Integer>(Integer::parseInt, Serdes.Integer().serializer());
+            case LONG:
+                return new SerializerWrapper<Long>(Long::parseLong, Serdes.Long().serializer());
+            case FLOAT:
+                return new SerializerWrapper<Float>(Float::parseFloat, Serdes.Float().serializer());
+            case DOUBLE:
+                return new SerializerWrapper<Double>(Double::parseDouble, Serdes.Double().serializer());
+            case BYTEARRAY:
+                return new SerializerWrapper<byte[]>(String::getBytes, Serdes.ByteArray().serializer());
+            case BYTEBUFFER:
+                return new SerializerWrapper<ByteBuffer>( s -> ByteBuffer.wrap(s.getBytes()), Serdes.ByteBuffer().serializer());
+            case BYTES:
+                return new SerializerWrapper<Bytes>(s -> Bytes.wrap(s.getBytes()), Serdes.Bytes().serializer());
+            case UUID:
+                return new SerializerWrapper<UUID>(UUID::fromString, Serdes.UUID().serializer());
+            default:
+                throw new UnsupportedOperationException("no serializer for Message type: " + type);
+        }
+    }
+    public class SerializerWrapper<T>{
+        private Function<String, T> function;
+        private Serializer<T> serializer;
+
+        public SerializerWrapper(Function<String, T> function, Serializer<T> serializer) {
+            this.function = function;
+            this.serializer = serializer;
+        }
     }
 }
 
