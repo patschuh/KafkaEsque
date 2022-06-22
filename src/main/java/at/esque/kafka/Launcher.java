59d6f3a571ea7beb2cda648312a92ccaa0a0299b
patschuh <46817726+patschuh@users.noreply.github.com>
2022-06-22 14:45:24 +0200

    configure logback

--- src/main/java/at/esque/kafka/Launcher.java
+++ src/main/java/at/esque/kafka/Launcher.java
@@ -1,6 +1,17 @@
 package at.esque.kafka;
 
+import ch.qos.logback.classic.util.ContextInitializer;
+
+import java.io.File;
+
 public class Launcher {
+
+    static {
+        File logbackFile = new File(String.format(System.getProperty("user.home") + "/.kafkaesque/%s", "/logback.xml"));
+        if (logbackFile.exists()) {
+            System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, logbackFile.getAbsolutePath());
+        }
+    }
 
     public static void main(String[] args) {
         Main.main(args);
